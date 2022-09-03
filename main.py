2a8ee3cf799fcb27ab7e5781ddc7bd2f6e2716e9
A-H-Mansoury <a.h.mansoury2002@gmail.com>
2022-09-03 10:59:11 +0430

    update main.py

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from Process import process
 from Visualize import visualize
 
-targets = [
-    '۱۷۱۲۳۱۵',#
-    #'۱۷۱۲۳۰۱',
-    '۱۷۱۴۳۰۳',#
-    '۱۷۱۴۳۹۶',#
-    #'۱۷۱۶۳۱۰',#
-    '۱۷۱۸۳۰۳',#
-    #'۱۷۳۲۲۰۴',
-    #'۱۷۳۲۳۰۳'#
-]
-name = 'temp'
+targets = """
+    ۱۷۱۲۳۱۵
+    ۱۷۱۴۳۰۳
+    ۱۷۱۴۳۹۶
+    ۱۷۱۶۳۱۰
+    ۱۷۱۸۳۰۳
+    ۱۷۳۲۲۰۴
+    ۱۷۳۲۳۰۳
+"""
 
+name = 'temp'
 
 golestan_html_path = './data/src.html'
 is_drive  = False
 
 if __name__ == '__main__':
+
+    targets = targets.strip().split()
     process.validate_targets(targets)
     p = process(targets, golestan_html_path)
     process_results = p.get_results()
