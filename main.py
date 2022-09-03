cfe8579b98904b77664e3a6943bf159cad741ec0
A-H-Mansoury <a.h.mansoury2002@gmail.com>
2022-09-03 17:49:43 +0430

    fix bugs

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from Process import process
 from Visualize import visualize
 
-targets = """
-    ۱۷۱۲۳۱۵
-    ۱۷۱۴۳۰۳
-    ۱۷۱۴۳۹۶
-    ۱۷۱۶۳۱۰
-    ۱۷۱۸۳۰۳
-    ۱۷۳۲۲۰۴
-    ۱۷۳۲۳۰۳
-"""
+import argparse
+import sys
 
-name = 'temp'
-
+def parse_args():
+    p = argparse.ArgumentParser(description='Schedule Maker')
+    p.add_argument('-t', '--targets', metavar='targets', type=str, help="a multiline string of course_ids")
+    p.add_argument('-n', '--name', metavar='name', type=str, help="a random name")
+    p.add_argument('-i', '--is_drive', metavar='is_drive', type=str, help="whether save results in google drive or not")
+    
+    if len(sys.argv) <= 1:
+        sys.argv.append('--help')
+    args = p.parse_args()
+    globals().update(vars(args))
+    
 golestan_html_path = './data/src.html'
-is_drive  = False
+
 
 if __name__ == '__main__':
-
+    parse_args()
     targets = targets.strip().split()
     process.validate_targets(targets)
     p = process(targets, golestan_html_path)
